1b067b2ad3d1e6a9b2eba50eab486377f285feaa
Martin Doušek <dousek.martin@gmail.com>
2023-05-13 15:37:06 +0200

    code refactor

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,6 +1,5 @@
 import styles from "./ErrorModal.module.css";
 import Backdrop from "./Backdrop";
-import Card from "./Card";
 import Button from "./Button";
 
 const ErrorModal = (props) => {
@@ -13,12 +12,6 @@ const ErrorModal = (props) => {
         </div>
         <div className={styles.invalid_input__content}>
           <p>{props.message}</p>
-          {/* <button
-            onClick={props.onConfirm}
-            className={styles.invalid_input__button}
-          >
-            Okay
-          </button> */}
           <Button className={styles.invalid_input__button} onClick={props.onConfirm}>Okay</Button>
         </div>
       </div>
